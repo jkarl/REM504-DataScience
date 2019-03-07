51638da69d48c227b2dfb016d3ae0256d372529a
pric3656 <46800013+pric3656@users.noreply.github.com>
2019-03-06 21:01:12 -0800

    Update Price_ggplot_excercise.R

--- GitHub_Practice/ggplot_assignment/Price_ggplot_excercise.R
+++ GitHub_Practice/ggplot_assignment/Price_ggplot_excercise.R
@@ -1,20 +1,32 @@
-mydata<-read.csv("C:/Users/sjpri/Dropbox/school/2019 Spring/data science/LPI_top-hit_any-hit.csv",header=T)
-head(mydata)
-names(mydata)
-
+library(readxl)
+library(tidyverse)
 library(ggplot2)
 library(dplyr)
-p<-ggplot(data=mydata,aes(x=mydata$X1ST_HIT,y=mydata$ANY_HIT,col=mydata$Veg,))+
+setwd('C:/Users/sjpri/Documents/GitHub/REM504-DataScience/GitHub_Practice/ggplot_assignment')
+mydata<-read.csv("C:/Users/sjpri/Documents/GitHub/REM504-DataScience/GitHub_Practice/ggplot_assignment/LPI_top-hit_any-hit.csv",header=T)
+#View(mydata)
+
+longdata <- mydata %>% gather(key=vege,value=cover,-Plot) #long form...I don't understand how or why this works
+longdata$key<-str_sub(longdata$vege,1,2) #need to create a new column named "key" to store our vege type
+longdata$method<-str_sub(longdata$vege,4,6) #need to create a new column named "method" to store our sample method
+data <- longdata[,-2] %>% spread(method, cover) #just splits up our two sample methods...I don't even know how
+names(data) <- c("Plot","vege","first","any") #renames our columns
+
+data$vege <- data$vege %>% #renaming variables within vege
+  gsub(pattern = "AG", replacement = "Annual Grass") %>%
+  gsub(pattern = "FB", replacement = "Forb") %>%
+  gsub(pattern = "PG", replacement = "Perennial Grass") %>%
+  gsub(pattern = "SH", replacement = "Shrub")
+
+
+ggplot(data,aes(x=first,y=any))+
   geom_point()+
-  #geom_abline(slope=1,intercept=0)+
-  facet_wrap(facets = ~mydata$Veg)+
-  #stat_smooth_func(geom="text",method="lm",hjust=0,parse=TRUE) +
-  geom_smooth(method="lm",se=FALSE)
-plot(p)
+  geom_abline(slope=1,intercept=0,colour='red',size=1,linetype='dashed')+
+  facet_wrap(facets = ~vege)+
+  geom_smooth(method="lm",se=FALSE,colour='black')+
+  xlab("Any Hit Cover") +
+  ylab("First Hit Cover") +
+  ggtitle("Vegetative Cover Indicators") +
+  theme(text = element_text(size = 12), plot.title = element_text(hjust = 0.5)) 
 
-
-p+geom_abline(slope=1,intercept=0)
-plot(p)
-
-
-
+ggsave("price_ggplot.tiff")
